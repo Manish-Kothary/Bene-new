610281b2197b5ab0bca3e98b975405daeefffc3b
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-10-31 19:17:13 +0530

    info section complete

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -5,28 +5,28 @@ export default class Info extends Component {
     return (
       <div
         className="container-fluid"
-        style={{ marginTop: "8%", paddingLeft: "1%", marginBottom: "8%" }}
+        style={{ marginTop: "8%", marginBottom: "8%" }}
       >
         <div className="row">
           <div className="col s12">
             <div className="row">
-              <div className="col s0 m0"></div>
+              <div className="col s0 m"></div>
               <div
-                className="col s12 m3"
+                className="col s12 m4"
                 style={{ paddingRight: "2%", textAlign: "right" }}
               >
-                <h1 style={{ fontSize: "950%", fontFamily: "montserrat" }}>
+                <h1 style={{ fontSize: "1250%", fontFamily: "montserrat" }}>
                   bene
                 </h1>
               </div>
               <div
                 className="col s12 m7"
-                style={{ paddingLeft: "0", paddingTop: "1.5%" }}
+                style={{ paddingLeft: "0", paddingTop: "2.3%" }}
               >
                 <h3
                   style={{
                     color: "#d2af78",
-                    fontSize: "400%",
+                    fontSize: "500%",
                     fontFamily: "Times new roman",
                     marginBottom: "0",
                     fontWeight: "549"
@@ -37,7 +37,7 @@ export default class Info extends Component {
                 <h3
                   style={{
                     color: "#d2af78",
-                    fontSize: "400%",
+                    fontSize: "500%",
                     fontFamily: "Times new roman",
                     marginTop: "0",
                     fontWeight: "549"
@@ -48,21 +48,21 @@ export default class Info extends Component {
               </div>
             </div>
             <div className="row">
-              <div className="col s12 m4">
+              <div className="col s12 m6">
                 <div
                   className="row"
-                  style={{ paddingLeft: "13.5%", paddingTop: "15%" }}
+                  style={{ paddingLeft: "14.6%", paddingTop: "15%" }}
                 >
                   <div className="col s12">
-                    <p style={{ fontFamily: "montserrat", fontSize: "100%" }}>
+                    <p style={{ fontFamily: "montserrat", fontSize: "139%" }}>
                       In our own quest to enhance total balance and reduce
                       stress, we
                     </p>
-                    <p style={{ fontFamily: "montserrat", fontSize: "100%" }}>
+                    <p style={{ fontFamily: "montserrat", fontSize: "139%" }}>
                       set out to understand, what is CBD. We met manufacturers
                       from
                     </p>
-                    <p style={{ fontFamily: "montserrat", fontSize: "100%" }}>
+                    <p style={{ fontFamily: "montserrat", fontSize: "139%" }}>
                       across the world. We spoke with doctors
                     </p>
                   </div>
@@ -70,8 +70,8 @@ export default class Info extends Component {
                 <div
                   className="row"
                   style={{
-                    marginTop: "30%",
-                    paddingLeft: "80%",
+                    marginTop: "10%",
+                    paddingLeft: "78.5%",
                     fontFamily: "montserrat",
                     fontSize: "110%"
                   }}
@@ -86,7 +86,7 @@ export default class Info extends Component {
                 </div>
               </div>
               <div className="col s12 m6">
-                <img src={bottle} alt="" srcset="" style={{ width: "80%" }} />
+                <img src={bottle} alt="" srcset="" style={{ width: "85%" }} />
               </div>
             </div>
           </div>
